db8c9d733ab82437ce280bb53ae3b80faa1604bd
CyberAngler93 <matthew.r.perry25@gmail.com>
2020-02-05 16:32:10 -0900

    feat: finishes frist 6 tests

--- stringCalc.cpp
+++ stringCalc.cpp
@@ -14,36 +14,70 @@ TEST_CASE("Testing empty string"){
 TEST_CASE("Testing empty single number"){
     CHECK(stringCalc("1") == 1);
     CHECK(stringCalc("13") == 13);
-    CHECK(stringCalc("-1") == -1);
+    CHECK(stringCalc("10") == 10);
     CHECK(stringCalc("3000") == 3000);
 }
 
-TEST_CASE("Testing empty single number"){
+TEST_CASE("Testing Two numbers seperated by a ,"){
     CHECK(stringCalc("1,1") == 2);
+    CHECK(stringCalc("3,1") == 4);
+    CHECK(stringCalc("1,3") == 4);
+    CHECK(stringCalc("0,0") == 0);
+    CHECK(stringCalc("2,3") == 5);
+    CHECK(stringCalc("444,444") == 888);
+}
+
+TEST_CASE("Testing Two numbers sepereated by \\n"){
+    CHECK(stringCalc("2\n2") == 4);
+    CHECK(stringCalc("5\n5") == 10);
+    CHECK(stringCalc("100\n2") == 102);
+    CHECK(stringCalc("2\n6") == 8);
+}
+
+TEST_CASE("Testing three numbers sepereated by \\n or ,"){
+    CHECK(stringCalc("2\n2,4") == 8);
+    CHECK(stringCalc("5\n5\n5") == 15);
+    CHECK(stringCalc("100\n2,100") == 202);
+    CHECK(stringCalc("2,6,100") == 108);
+    CHECK(stringCalc("2,2\n2") == 6);
+}
+
+TEST_CASE("Testing negative numbers"){
 
 }
+
+
 
 int stringCalc( const std::string & userInput){
     if(userInput == ""){
         return 0;
     }
-    auto found = userInput.find(',');
-    if(found == std::string::npos){
-        std::string pre,post = "";
-        int commacount = 0;
+    auto found = userInput.find_first_of(",\n");
+    if(found != std::string::npos){
+        std::string first,second,third = "";
+        int delimcount = 0;
         for(auto i = 0; i < userInput.size(); i++){
-            if(userInput[i] == ','){
-                commacount++;
+            if(userInput[i] == ',' || userInput[i] == '\n'){
+                delimcount++;
             }
-            else if(commacount< 1){
-                pre = pre + userInput[i];
+            else if(delimcount< 1){
+                first = first + userInput[i];
             }
-            else if(commacount == 1){
-                post = post + userInput[i];
+            else if(delimcount == 1){
+                second = second + userInput[i];
+            }
+            else if(delimcount == 2){
+                third = third + userInput[i];
             }
         }
-        int total = std::stoi(pre) + std::stoi(post);
-        return total;
+        if(delimcount >= 2){
+            int total = std::stoi(first) + std::stoi(second) + std::stoi(third);
+            return total;
+        }
+        if(delimcount == 1){
+            int total = std::stoi(first) + std::stoi(second);
+            return total;
+        }
     }
     int count = std::stoi(userInput);
     return count;  
